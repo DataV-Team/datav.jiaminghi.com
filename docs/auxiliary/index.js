ed6ec379d7fd570ba632d235a54b1673d3046287
jiaming743 <743192023@qq.com>
2019-01-02 20:26:54 +0800

    optmization

--- docs/auxiliary/index.js
+++ docs/auxiliary/index.js
@@ -1,5 +1,7 @@
 import clickToCopy from './clickToCopy/index.vue'
+import foldBox from './foldBox/index.vue'
 
 export default function (Vue) {
   Vue.component('clickToCopy', clickToCopy)
+  Vue.component('foldBox', foldBox)
 }
