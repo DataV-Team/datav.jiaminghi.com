e1d12f90e2769ecf7f1e87aaae114a76f4b4fa34
jiaming743 <743192023@qq.com>
2019-01-06 12:26:02 +0800

    add enhance config

--- docs/DataV/mixins/colorsMixin.js
+++ docs/DataV/mixins/colorsMixin.js
@@ -1,9 +1,10 @@
-import defaultColors from '../config/color.js'
-
 export default {
   data () {
     return {
-      defaultColors,
+      defaultColors: [
+        '#9cf4a7', '#66d7ee', '#eee966',
+        '#a866ee', '#ee8f66', '#ee66aa'
+      ],
 
       drawColors: '',
 
