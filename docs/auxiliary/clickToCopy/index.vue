0ad7658fc0532c0dfa4d1f7245875b262d892430
jiaming743 <743192023@qq.com>
2019-01-06 16:58:04 +0800

    add copy tip

--- docs/auxiliary/clickToCopy/index.vue
+++ docs/auxiliary/clickToCopy/index.vue
@@ -1,13 +1,30 @@
 <template>
-  <a class="click-to-copy" :info="info" @click="copy">点击复制</a>
+  <a class="click-to-copy" :info="info" @click="!copyAni && copy()">
+    <span :class="`ax-ctc-btn ${copyAni && 'fade'}`">点击复制</span>
+    <svg :class="`ax-svg-container ${copyAni && 'show'}`">
+      <polyline :points="`5, 8 12, 15 25, 5`" />
+    </svg>
+  </a>
 </template>
 
 <script>
 export default {
   name: 'ClickToCopy',
-  props: ['info'],
+  props: ['info', 'btnTip'],
+  data () {
+    return {
+      copyAni: false
+    }
+  },
   methods: {
     copy () {
+      const { doCopyAction, doCopyTip } = this
+
+      doCopyAction()
+
+      doCopyTip()
+    },
+    doCopyAction () {
       const { info } = this
 
       const inputEle = document.createElement('input')
@@ -21,6 +38,13 @@ export default {
       document.execCommand('copy')
 
       inputEle.parentNode.removeChild(inputEle)
+    },
+    doCopyTip () {
+      this.copyAni = true
+
+      setTimeout(() => {
+        this.copyAni = false
+      }, 2000)
     }
   }
 }
@@ -28,6 +52,43 @@ export default {
 
 <style lang="less">
 .click-to-copy {
+  position: relative;
   cursor: pointer;
+
+  .fade {
+    opacity: 0 !important;
+  }
+
+  .show {
+    opacity: 1 !important;
+
+    polyline {
+      stroke-dasharray: 30 30 !important;
+    }
+  }
+
+  .ax-ctc-btn {
+    opacity: 1;
+    transition: all 0.3s;
+  }
+
+  .ax-svg-container {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 30px;
+    height: 20px;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    transition: all 0.3s;
+
+    polyline {
+      fill: none;
+      stroke: #46bd87;
+      stroke-width: 3;
+      stroke-dasharray: 0 30;
+      transition: all 1s;
+    }
+  }
 }
 </style>
