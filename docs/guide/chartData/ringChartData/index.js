41acbd904c89895cd9c4753d6a15f794622eab22
jiaming743 <743192023@qq.com>
2019-01-11 16:09:54 +0800

    add ring chart data

---
+++ docs/guide/chartData/ringChartData/index.js
@@ -0,0 +1,8 @@
+import ringChartData1 from './ringChartData1'
+import ringChartData2 from './ringChartData2'
+
+export default {
+  ringChartTag: `<ring-chart :data="data" :labelLine="labelLine" :colors="colors" />`,
+  ringChartData1,
+  ringChartData2
+}
