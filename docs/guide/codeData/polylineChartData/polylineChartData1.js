accf16c630276ba97a31af4cef9bd67d58f016c7
jiaming743 <743192023@qq.com>
2019-01-16 15:33:32 +0800

    some modify

--- docs/guide/codeData/polylineChartData/polylineChartData1.js
+++ docs/guide/codeData/polylineChartData/polylineChartData1.js
@@ -4,12 +4,12 @@ export default {
       value: [32, 30, 55, 24, 33, 25],
       lineColor: '#30e8d9',
       pointColor: '#eee996',
+      valueTextColor: 'inherit',
     }
   ],
   x: {
     tags: ['西峡', '周口', '南阳', '驻马店', '郑州', '洛阳']
   },
   showValueText: true,
-  valueTextColor: 'inherit',
   valueTextOffset: [10, -5]
 }
--- docs/guide/codeData/polylineChartData/polylineChartData2.js
+++ docs/guide/codeData/polylineChartData/polylineChartData2.js
@@ -4,13 +4,13 @@ export default {
       value: [32, 30, 55, 24, 33, 25],
       lineColor: '#30e8d9',
       pointColor: '#eee996',
-      type: 'smoothline'
+      type: 'smoothline',
+      valueTextColor: 'inherit'
     }
   ],
   x: {
     tags: ['西峡', '周口', '南阳', '驻马店', '郑州', '洛阳']
   },
   showValueText: true,
-  valueTextColor: 'inherit',
   valueTextOffset: [10, -5]
 }
