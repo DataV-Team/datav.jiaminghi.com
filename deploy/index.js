538ef1e305ec58219044274a09b9fe88cafcbc7f
jiaming743 <743192023@qq.com>
2019-08-22 15:15:30 +0800

    add auto deploy process

---
+++ deploy/index.js
@@ -0,0 +1,83 @@
+const { fileForEach } = require('@jiaminghi/fs')
+const Client = require('ftp')
+const print = require('./plugin/print')
+const { emptyDir, put, mkDir } = require('./plugin/ftp')
+const getNodeParams = require('./plugin/nodeParams')
+
+let config = null
+
+try {
+  config = require('./config')
+} catch (err) {
+  void 0
+}
+
+const DIST_PATH = './docs/.vuepress/dist'
+
+const ftp = new Client()
+
+ftp.on('ready', async foo => {
+  print.tip('FTP connected!')
+
+  const isEmpty = await emptyDir(ftp, '/', ['demo'])
+
+  if (!isEmpty) {
+    print.error('Exception in emptyDir!')
+
+    return false
+  }
+
+  let status = true
+
+  await fileForEach(DIST_PATH, async src => {
+    const destPath = '/' + src.split('dist/')[1]
+    const destDir = destPath.split('/').slice(0, -1).join('/')
+
+    await mkDir(ftp, destDir, true)
+
+    print.tip('Upload: ' + destPath)
+
+    if (!await put(ftp, src, destPath)) {
+      status = false
+      
+      print.error('Exception in upload ' + destPath)
+    }
+  })
+
+  if (status) {
+    print.yellow('-------------------------------------')
+    print.success('    Automatic Deployment Success!    ')
+    print.yellow('-------------------------------------')  
+  }
+
+  ftp.destroy()
+})
+
+ftp.on('greeting', foo => {
+  print.tip('FTP greeting')
+})
+ftp.on('close', foo => {
+  print.tip('FTP close')
+})
+ftp.on('end', foo => {
+  print.tip('FTP end')
+})
+ftp.on('error', foo => {
+  print.tip('FTP error')
+})
+
+const { host, user, pass } = config || getNodeParams()
+
+if (!host || !user || !pass) {
+  print.error('Upload Dist to FTP Missing Parameters!')
+
+  return false
+}
+
+print.tip('Start Upload!')
+
+ftp.connect({
+  host,
+  user,
+  password: pass
+})
