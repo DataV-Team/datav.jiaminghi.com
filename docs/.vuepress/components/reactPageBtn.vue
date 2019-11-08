d938296d1127c76f9843eaa80eb9d8f0c4b82cc6
JiaMing <743192023@qq.com>
2019-11-08 16:27:28 +0800

    add new cmp for auto turn react home page

---
+++ docs/.vuepress/components/reactPageBtn.vue
@@ -0,0 +1,27 @@
+<template>
+  <span class="react-page-btn">
+    （<span class="green" @click="turnToReactPage">React版<OutboundLink /></span>）
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'ReactPageBtn',
+  methods: {
+    turnToReactPage () {
+      const { pathname } = location
+
+      window.open(`http://datav-react.jiaminghi.com${pathname}`)
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.react-page-btn .green {
+  color: #3eaf7c;
+  cursor: pointer;
+  font-weight: 500;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+}
+</style>
