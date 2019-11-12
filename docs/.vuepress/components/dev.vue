fd9cd7a26020ff8a79aceb16abce0c4c93a402db
JiaMing <743192023@qq.com>
2019-11-12 11:51:33 +0800

    update 2019-11-12

--- docs/.vuepress/components/dev.vue
+++ docs/.vuepress/components/dev.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="dev-container">
-    <dv-active-ring-chart :config="config" style="width:300px;height:300px" />
+    <dv-scroll-ranking-board :config="config" style="width:300px;height:300px" />
   </div>
 </template>
 
@@ -11,32 +11,32 @@ export default {
     return {
       config: {
         data: [
-          {
-            name: '周口',
-            value: 55
-          },
-          {
-            name: '南阳',
-            value: 120
-          },
-          {
-            name: '西峡',
-            value: 78
-          },
-          {
-            name: '驻马店',
-            value: 66
-          },
-          {
-            name: '新乡',
-            value: 80
-          }
+          ['行1列1', '行1列2', '行1列3'],
+          ['行2列1', '行2列2', '行2列3'],
+          ['行3列1', '行3列2', '行3列3'],
+          ['行4列1', '行4列2', '行4列3'],
+          ['行5列1', '行5列2', '行5列3'],
+          ['行6列1', '行6列2', '行6列3'],
+          ['行7列1', '行7列2', '行7列3'],
+          ['行8列1', '行8列2', '行8列3'],
+          ['行9列1', '行9列2', '行9列3'],
+          ['行10列1', '行10列2', '行10列3']
         ]
       }
     }
   },
-  created () {
+  async mounted () {
+    let i = 0
+    while (i < 10) {
+      console.log(i)
+      await new Promise((resolve, reject) => {
+        setTimeout(resolve, Math.floor(Math.random() * 1000) + 211)
+      })
 
+      i++
+
+      this.config = { ...this.config, data: [...this.config.data] }
+    }
   }
 }
 </script>
