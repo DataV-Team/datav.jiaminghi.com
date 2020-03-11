551b404f5b44a135e0e100801bead7aacef1e84d
JM <743192023@qq.com>
2020-03-11 18:59:18 +0800

    update 2020-03-11

--- docs/.vuepress/components/dev.vue
+++ docs/.vuepress/components/dev.vue
@@ -1,34 +1,19 @@
 <template>
   <div id="dev-container">
-    <dv-active-ring-chart :config="config" style="width:300px;height:300px" />
+    <dv-charts :option="option" style="width:300px;height:300px" />
   </div>
 </template>
 
 <script>
+let option = `{"legend":{"data":[{"name":"下订单数量","color":"#ff5ca9"}],"textStyle":{"fill":"#fff"}},"xAxis":["2018-08-05","2018-08-06","2018-08-07","2018-08-08","2018-08-09"],"yAxis":{"data":"value","splitLine":{"show":false},"axisLine":{"style":{"stroke":"#999"}},"axisLabel":{"style":{"fill":"#999"}},"axisTick":{"show":false}},"series":[{"name":"下订单数量","data":[1,2,3,4,5],"type":"line","lineStyle":{"stroke":"#ff5ca9"},"lineArea":{"show":true,"gradient":["rgba(55, 162, 218, 0.6)","rgba(55, 162, 218, 0)"]},"linePoint":{"radius":4,"style":{"fill":"#ff5ca9","stroke":"transparent"}}}]}`
+
+option = JSON.parse(option)
+
 export default {
   name: 'dev',
   data () {
     return {
-      config: {
-        color: ['#e8dd82','#ff34a3','#c716e8','#4ae814','#100de8','#e80700','#797B7F','#CFB2A9','#DE9325','#7FAE90','#9FDABF','#E98F6F','#6AB0B8','#334B5C','#fc0503'],
-        lineWidth: 30,
-        radius: '55%',
-        activeRadius: '60%',
-        digitalFlopToFixed: 2,
-        data: [
-          { value: 1799, name: "公安"},
-          { value: 1789, name: "人社"},
-          { value: 1043, name: "医保"},
-          { value: 787, name: "住建"},
-          { value: 587, name: "总工会"},
-          { value: 251, name: "档案"},
-          { value: 136, name: "民政"},
-          { value: 46, name: "卫健"},
-          { value: 35, name: "残联"},
-          { value: 19, name: "公安1"},
-          { value: 7, name: "粮食"}
-        ]
-      }
+      option
     }
   }
 }
