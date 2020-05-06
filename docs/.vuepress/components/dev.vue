8db66a16ed5dce8aeae42424e2ffb95f7c551ca5
jiaming743 <743192023@qq.com>
2020-05-06 15:12:43 +0800

    update 2020-05-06

--- docs/.vuepress/components/dev.vue
+++ docs/.vuepress/components/dev.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="dev-container">
-    <dv-capsule-chart :config="config" style="width:300px;height:300px" />
+    <dv-active-ring-chart :config="config" style="width:300px;height:300px" />
   </div>
 </template>
 
@@ -10,33 +10,7 @@ export default {
   name: 'dev',
   data () {
     return {
-      config: {
-        data: [
-          {
-            name: '南阳',
-            value: 167
-          },
-          {
-            name: '周口',
-            value: 123
-          },
-          {
-            name: '漯河',
-            value: 98
-          },
-          {
-            name: '郑州',
-            value: 75
-          },
-          {
-            name: '西峡',
-            value: 66
-          },
-        ],
-        colors: ['#e062ae', '#fb7293', '#e690d1', '#32c5e9', '#96bfff'],
-        unit: '单位',
-        showValue: true
-      }
+      config: { data: [ { name: '周口', value: 16984 }, { name: '南阳', value: 48872 }, { name: '西峡', value: 17366 }, { name: '驻马店', value: 6101 }, { name: '新乡', value: 3197 }, { name: '郑州', value: 97 } ], showValue: true, fontSize: 14, digitalFlopToFixed: 2 }
     }
   }
 }
