a3238551a36b00a5f513aa5e15476ee1f016fa3a
jiaming743 <743192023@qq.com>
2020-05-06 17:59:36 +0800

    添加关键字

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,7 @@ module.exports = {
   title: 'DataV',
   description: 'Vue 大屏数据展示组件库',
   head: [
+    ['meta', { name: 'keywords', content: 'datav,data,bi,开源,免费,大屏' }],
     ['link', { rel: 'icon', href: '/favicon.ico' }]
   ],
   host: 'localhost',
