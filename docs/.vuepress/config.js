49b77ce99d1dfd9c3db8480e6c66526ddf0e0a1c
JiaMing <743192023@qq.com>
2023-01-04 23:37:06 +0800

    docs: add vue3 & react doc link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,60 +1,73 @@
 module.exports = {
-  title: 'DataV',
-  description: 'Vue 大屏数据展示组件库',
+  title: "DataV",
+  description: "Vue 大屏数据展示组件库",
   head: [
-    ['meta', { name: 'keywords', content: 'datav,data,bi,开源,免费,大屏' }],
-    ['link', { rel: 'icon', href: '/favicon.ico' }]
+    ["meta", { name: "keywords", content: "datav,data,bi,开源,免费,大屏" }],
+    ["link", { rel: "icon", href: "/favicon.ico" }],
   ],
-  host: 'localhost',
+  host: "localhost",
   port: 5000,
   themeConfig: {
     nav: [
       {
-        text: '指南',
-        link: '/guide/'
+        text: "指南",
+        link: "/guide/",
       },
       {
-        text: '支持',
-        link: '/support/'
-      },
-      {
-        text: 'Demo',
-        link: '/demo/'
-      },
-      {
-        text: 'GitHub',
+        text: "Vue3 & React版本",
         items: [
           {
-            text: '项目源码仓库',
-            link: 'https://github.com/DataV-Team/Datav'
+            text: "Vue3版本",
+            link: "https://datav-vue3.jiaminghi.com/",
           },
           {
-            text: '文档及Demo源码仓库',
-            link: 'https://github.com/DataV-Team/datav.jiaminghi.com'
-          }
-        ]
-      }
+            text: "React版本",
+            link: "http://datav-react.jiaminghi.com/",
+          },
+        ],
+      },
+      {
+        text: "支持",
+        link: "/support/",
+      },
+      {
+        text: "Demo",
+        link: "/demo/",
+      },
+      {
+        text: "GitHub",
+        items: [
+          {
+            text: "项目源码仓库",
+            link: "https://github.com/DataV-Team/Datav",
+          },
+          {
+            text: "文档及Demo源码仓库",
+            link: "https://github.com/DataV-Team/datav.jiaminghi.com",
+          },
+        ],
+      },
     ],
     sidebar: {
-      '/guide/': [
-        '',
-        'fullScreenContainer',
-        'loading',
-        'borderBox',
-        'decoration',
-        'charts',
-        'activeRingChart',
-        'capsuleChart',
-        'waterLevelPond',
-        'percentPond',
-        'flylineChart',
-        'flylineChartEnhanced',
-        'conicalColumnChart',
-        'digitalFlop',
-        'scrollBoard',
-        'scrollRankingBoard'
+      "/guide/": [
+        "",
+        "fullScreenContainer",
+        "loading",
+        "borderBox",
+        "decoration",
+        "charts",
+        "activeRingChart",
+        "capsuleChart",
+        "waterLevelPond",
+        "percentPond",
+        "flylineChart",
+        "flylineChartEnhanced",
+        "conicalColumnChart",
+        "digitalFlop",
+        "scrollBoard",
+        "scrollRankingBoard",
       ],
-      '/dev/': ['']
-    }
-  }
-}
+      "/dev/": [""],
+    },
+  },
+};
